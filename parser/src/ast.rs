6e03b6f9e3783379a755c23299d6cd956fa813d2
khigasa <kojihigasa@gmail.com>
2019-02-10 16:18:50 +0900

    Add stmt nodes

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -18,18 +18,6 @@ pub struct Program {
 
 pub type LocatedStatement = Located<Statement>;
 
-pub struct Located<T> {
-    pub location: Location,
-    pub node: T,
-}
-
-pub struct Located<T> {
-    pub location: Location,
-    pub node: T,
-}
-
-
-
 #[derive(Debug, PartialEq)]
 pub struct Located<T> {
     pub location: Location,
@@ -47,5 +35,104 @@ pub struct SingleImport {
 /// Ast nodes for elegua statements.
 #[derive(Debug, PartialEq)]
 pub enum Statement {
+    Break,
+    Continue,
+    Return {
+        value: Option<Vec<Expression>>,
+    },
+    Import {
+        import_parts: Vec<SingleImport>,
+    },
+    Assign {
+        targets: Vec<Expression>,
+        value: Expression,
+    },
+    AugAssign {
+        target: Box<Expression>,
+        op: Operator,
+        value: Box<Expression>,
+    },
+    Expression {
+        expression: Expression,
+    },
+    Global {
+        names: Vec<String>,
+    },
+    If {
+        test: Expression,
+        body: Vec<LocatedStatement>,
+        orelse: Option<Vec<LocatedStatement>>,
+    },
+    While {
+        test: Expression,
+        body: Vec<LocatedStatement>,
+        orelse: Option<Vec<LocatedStatement>>,
+    },
+    For {
+        target: Expression,
+        iter: Vec<Expression>,
+        body: Vec<LocatedStatement>,
+        orelse: Option<Vec<LocatedStatement>>,
+    },
+    ClassDef {
+        name: String,
+        body: Vec<LocatedStatement>,
+    },
+    FunctionDef {
+        name: String,
+        args: Parameters,
+        body: Vec<LocatedStatement>,
+    },
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expression {
+    
+}
+
+#[derive(Debug, PartialEq, Default)]
+pub struct Parameters {
+    pub args: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BooleanOperator {
+    And,
+    Or,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum UnaryOperator {
+    Pos,
+    Neg,
+    Not,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum UnaryOperator {
+    Equal,
+    NotEqual,
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Is,
+    IsNot,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Number {
+    Integer { value: BigInt },
+    Float { value: f64 },
 }
 
